3238a732086236f52397f3f1a0798e355b90ccbb
Daehyun Kim <hatemogi@gmail.com>
2015-05-10 21:58:40 +0900

    01, 02, 03 1차 번역

--- src/koans/02_lists.clj
+++ src/koans/02_lists.clj
@@ -2,43 +2,55 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "Lists can be expressed by function or a quoted form"
+  ; "Lists can be expressed by function or a quoted form"
+  "리스트는 함수 또는 인용된 형태로 표현할 수 있다"
   (= '(__ __ __ __ __) (list 1 2 3 4 5))
 
-  "They are Clojure seqs (sequences), so they allow access to the first"
+  ; "They are Clojure seqs (sequences), so they allow access to the first"
+  "리스트는 클로저 순서형(sequences)이므로, 제일 처음 값을 쉽게 읽을 수 있다"
   (= __ (first '(1 2 3 4 5)))
 
-  "As well as the rest"
+  ; "As well as the rest"
+  "뒷부분 값들을 접근하기도 쉽다"
   (= __ (rest '(1 2 3 4 5)))
 
-  "Count your blessings"
+  ; "Count your blessings"
+  "너의 축복을 세어 보라"
   (= __ (count '(dracula dooku chocula)))
 
-  "Before they are gone"
+  ; "Before they are gone"
+  "그 것들이 사라지기 전에"
   (= __ (count '()))
 
-  "The rest, when nothing is left, is empty"
+  ; "The rest, when nothing is left, is empty"
+  "아무 것도 남지 않았을 때의 나머지는 빈 것이니라"
   (= __ (rest '(100)))
 
-  "Construction by adding an element to the front is easy"
+  ; "Construction by adding an element to the front is easy"
+  "리스트 맨앞에 요소를 추가해서 만들기 쉽다"
   (= __ (cons :a '(:b :c :d :e)))
 
-  "Conjoining an element to a list isn't hard either"
+  ; "Conjoining an element to a list isn't hard either"
+  "리스트 뒤에 요소를 합치는 것 역시 어렵지 않다"
   (= __ (conj '(:a :b :c :d) :e))
 
-  "You can use a list like a stack to get the first element"
+  ; "You can use a list like a stack to get the first element"
+  "리스트를 스택처럼 쓰면서 맨 앞의 요소를 가져올 수 있다"
   (= __ (peek '(:a :b :c :d :e)))
 
-  "Or the others"
+  ; "Or the others"
+  "아니면 그 나머지 값들도"
   (= __ (pop '(:a :b :c :d :e)))
 
-  "But watch out if you try to pop nothing"
+  ; "But watch out if you try to pop nothing"
+  "그러나, 빈 것에서 꺼낼 때는 조심하라"
   (= __ (try
           (pop '())
           (catch IllegalStateException e
             "No dice!")))
 
-  "The rest of nothing isn't so strict"
+  ; "The rest of nothing isn't so strict"
+  "그러나 나머지를 구하는 것은 그렇게 엄격하지는 않다"
   (= __ (try
           (rest '())
           (catch IllegalStateException e
--- src/koans/03_vectors.clj
+++ src/koans/03_vectors.clj
@@ -2,32 +2,42 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "You can use vectors in clojure as array-like structures"
+  ; "You can use vectors in clojure as array-like structures"
+  "클로저의 벡터(vector)는 마치 배열처럼 쓸 수 있다"
   (= __ (count [42]))
 
-  "You can create a vector from a list"
+  ; "You can create a vector from a list"
+  "리스트를 벡터로 만들 수도 있다"
   (= __ (vec '(1)))
 
-  "Or from some elements"
+  ; "Or from some elements"
+  "아니면 요소 몇 개로부터"
   (= __ (vector nil nil))
 
-  "But you can populate it with any number of elements at once"
+  ; "But you can populate it with any number of elements at once"
+  "아니면 여러 요소를 한번에 가져와서 만들 수도 있다"
   (= [1 __] (vec '(1 2)))
 
-  "Conjoining to a vector is different than to a list"
+  ; "Conjoining to a vector is different than to a list"
+  "벡터에 어떤 요소를 결합(conjoin)하는 것은 리스트에 결합하는 방식과 다르다"
   (= __ (conj [111 222] 333))
 
-  "You can get the first element of a vector like so"
+  ; "You can get the first element of a vector like so"
+  "벡터의 첫번째 요소는 리스트에서 가져올 때와 같이 구할 수 있다"
   (= __ (first [:peanut :butter :and :jelly]))
 
-  "And the last in a similar fashion"
+  ; "And the last in a similar fashion"
+  "비슷한 방식으로 마지막 값도 구할 수 있다"
   (= __ (last [:peanut :butter :and :jelly]))
 
-  "Or any index if you wish"
+  ; "Or any index if you wish"
+  "또, 몇 번째 요소를 원하는지 말하라"
   (= __ (nth [:peanut :butter :and :jelly] 3))
 
-  "You can also slice a vector"
+  ; "You can also slice a vector"
+  "벡터를 쪼갤 수도 있다"
   (= __ (subvec [:peanut :butter :and :jelly] 1 3))
 
-  "Equality with collections is in terms of values"
+  ; "Equality with collections is in terms of values"
+  "컬렉션과 비교는 각각의 값을 비교함으로 이뤄진다"
   (= (list 1 2 3) (vector 1 2 __)))
