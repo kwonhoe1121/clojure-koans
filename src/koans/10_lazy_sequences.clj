9225da580581be4614cedf8f08962874e540f37a
Daehyun Kim <hatemogi@gmail.com>
2015-06-13 00:04:36 +0900

    10, 11, 12 1차 번역

--- src/koans/10_lazy_sequences.clj
+++ src/koans/10_lazy_sequences.clj
@@ -2,27 +2,34 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "There are many ways to generate a sequence"
+  ;; "There are many ways to generate a sequence"
+  "시퀀스를 만드는 방법은 다양하다"
   (= __ (range 1 5))
 
-  "The range starts at the beginning by default"
+  ;; "The range starts at the beginning by default"
+  "범위는 기본적으로 0에서 시작한다"
   (= __ (range 5))
 
-  "Only take what you need when the sequence is large"
+  ;; "Only take what you need when the sequence is large"
+  "시퀀스가 클 때는 필요한 만큼만 가져온다"
   (= [0 1 2 3 4 5 6 7 8 9]
      (take __ (range 100)))
 
-  "Or limit results by dropping what you don't need"
+  ;; "Or limit results by dropping what you don't need"
+  "아니면, 필요 없는 만큼을 버리고 쓴다"
   (= [95 96 97 98 99]
      (drop __ (range 100)))
 
-  "Iteration provides an infinite lazy sequence"
+  ;; "Iteration provides an infinite lazy sequence"
+  "iterate로 무한 지연(lazy) 시퀀스를 만들 수 있다"
   (= __ (take 20 (iterate inc 0)))
 
-  "Repetition is key"
+  ;; "Repetition is key"
+  "반복이 핵심이다"
   (= [:a :a :a :a :a :a :a :a :a :a ]
      (repeat 10 __))
 
-  "Iteration can be used for repetition"
+  ;; "Iteration can be used for repetition"
+  "이터레이션을 반복하기 위해 쓸 수도 있다"
   (= (repeat 100 :foo)
      (take 100 (iterate ___ :foo))))
--- src/koans/12_creating_functions.clj
+++ src/koans/12_creating_functions.clj
@@ -4,32 +4,39 @@
 (defn square [x] (* x x))
 
 (meditations
-  "One may know what they seek by knowing what they do not seek"
+  ;; "One may know what they seek by knowing what they do not seek"
+  "찾지 않는 것을 앎으로써 찾는 것을 알게 될 수 있다"
   (= [__ __ __] (let [not-a-symbol? (complement symbol?)]
                   (map not-a-symbol? [:a 'b "c"])))
 
-  "Praise and 'complement' may help you separate the wheat from the chaff"
+  ;; "Praise and 'complement' may help you separate the wheat from the chaff"
+  "'complement'로 밀의 겨를 벗겨낼 수 있다"
   (= [:wheat "wheat" 'wheat]
        (let [not-nil? ___]
          (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
 
-  "Partial functions allow procrastination"
+  ;; "Partial functions allow procrastination"
+  "부분(partial) 함수로 늑장 부릴 수 있다"
   (= 20 (let [multiply-by-5 (partial * 5)]
           (___ __)))
 
-  "Don't forget: first things first"
+  ;; "Don't forget: first things first"
+  "명심하라: 처음 것이 먼저다"
   (= [__ __ __ __]
        (let [ab-adder (partial concat [:a :b])]
          (ab-adder [__ __])))
 
-  "Functions can join forces as one 'composed' function"
+  ;; "Functions can join forces as one 'composed' function"
+  "여러 함수를 하나의 합성(composed) 함수로 합칠 수 있다"
   (= 25 (let [inc-and-square (comp square inc)]
           (inc-and-square __)))
 
-  "Have a go on a double dec-er"
+  ;; "Have a go on a double dec-er"
+  "이중 차감함수를 만들어 보라"
   (= __ (let [double-dec (comp dec dec)]
           (double-dec 10)))
 
-  "Be careful about the order in which you mix your functions"
+  ;; "Be careful about the order in which you mix your functions"
+  "함수를 합치는 순서에 유의하라"
   (= 99 (let [square-and-dec ___]
           (square-and-dec 10))))
