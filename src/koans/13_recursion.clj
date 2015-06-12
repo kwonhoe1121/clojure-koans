95799d9483ad75a4384e9a70d552d1207af7be4f
Daehyun Kim <hatemogi@gmail.com>
2015-06-13 01:48:25 +0900

    13, 14, 15 1차 번역

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -20,35 +20,46 @@
   __)
 
 (meditations
-  "Recursion ends with a base case"
+  ;; "Recursion ends with a base case"
+  "재귀는 종료 기준이 있다"
   (= true (is-even? 0))
 
-  "And starts by moving toward that base case"
+  ;; "And starts by moving toward that base case"
+  "그리고 그 종료 기준을 위해 나아가며 시작한다"
   (= false (is-even? 1))
 
-  "Having too many stack frames requires explicit tail calls with recur"
+  ;; "Having too many stack frames requires explicit tail calls with recur"
+  "너무 많은 스택 프레임이 필요한 경우 recur로 명시적인 꼬리재귀호출(tail call)이 필요하다"
   (= false (is-even-bigint? 100003N))
 
-  "Reversing directions is easy when you have not gone far"
+  ;; "Reversing directions is easy when you have not gone far"
+  "너무 멀리 가지 않았다면 되돌아가기 쉽다"
   (= '(1) (recursive-reverse [1]))
 
-  "Yet it becomes more difficult the more steps you take"
+  ;; "Yet it becomes more difficult the more steps you take"
+  "더 걸어갈 수록 힘들어지지만 말이다"
   (= '(5 4 3 2 1) (recursive-reverse [1 2 3 4 5]))
 
-  "Simple things may appear simple."
+  ;; "Simple things may appear simple."
+  "간단한 것들은 겉보기에도 간단하다"
   (= 1 (factorial 1))
 
-  "They may require other simple steps."
+  ;;"They may require other simple steps."
+  "간단한 몇 단계를 더 필요로 하기도 한다"
   (= 2 (factorial 2))
 
-  "Sometimes a slightly bigger step is necessary"
+  ;; "Sometimes a slightly bigger step is necessary"
+  "어떨 때는 조금 큰 단계가 필요하다"
   (= 6 (factorial 3))
 
-  "And eventually you must think harder"
+  ;; "And eventually you must think harder"
+  "그리고 결국은 열심히 생각해야만 한다"
   (= 24 (factorial 4))
 
-  "You can even deal with very large numbers"
+  ;; "You can even deal with very large numbers"
+  "매우 큰 숫자를 다룰 수도 있겠지만"
   (< 1000000000000000000000000N (factorial 1000N))
 
-  "But what happens when the machine limits you?"
+  ;; "But what happens when the machine limits you?"
+  "기계가 더 이상 받쳐주지 않을 때는 어떻게 할 것인가?"
   (< 1000000000000000000000000N (factorial 100003N)))
--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -7,38 +7,45 @@
    :state "TX"})
 
 (meditations
-  "Destructuring is an arbiter: it breaks up arguments"
+  ;; "Destructuring is an arbiter: it breaks up arguments"
+  "디스트럭쳐링(destructuring)으로 인수를 쪼갤 수 있다"
   (= __ ((fn [[a b]] (str b a))
          [:foo :bar]))
 
-  "Whether in function definitions"
+  ;; "Whether in function definitions"
+  "함수 정의에서도 쓸 수 있고"
   (= (str "First comes love, "
           "then comes marriage, "
           "then comes Clojure with the baby carriage")
      ((fn [[a b c]] __)
       ["love" "marriage" "Clojure"]))
 
-  "Or in let expressions"
+  ;; "Or in let expressions"
+  "또는 let 표현식에서도 쓴다"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
        __))
 
-  "You can regain the full argument if you like arguing"
+  ;; "You can regain the full argument if you like arguing"
+  "전체 인수를 따로 받을 수도 있다"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
        __))
 
-  "Break up maps by key"
+  ;; "Break up maps by key"
+  "맵(map)을 키별로 나눠 받을 수 있다"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
        __))
 
-  "Or more succinctly"
+  ;; "Or more succinctly"
+  "아니면 더 간명하게도"
   (= "123 Test Lane, Testerville, TX"
      (let [{:keys [street-address __ __]} test-address]
        __))
 
-  "All together now!"
+  ;; "All together now!"
+  "전부 다 써보자!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
      (___ ["Test" "Testerson"] test-address)))
