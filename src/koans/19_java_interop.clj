0308770160f088c650e5e3b7cb42b48d61309940
Daehyun Kim <hatemogi@gmail.com>
2015-06-14 23:46:37 +0900

    19, 20, 21 1차 번역

--- src/koans/19_java_interop.clj
+++ src/koans/19_java_interop.clj
@@ -2,18 +2,23 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "You may have done more with Java than you know"
+  ;; "You may have done more with Java than you know"
+  "네가 알던 것보다 더 많이 자바로 해왔다"
   (= __ (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
 
-  "The dot signifies easy and direct Java interoperation"
+  ;; "The dot signifies easy and direct Java interoperation"
+  "마침표로 자바 연동을 쉽고 직접적으로 한다"
   (= __ (.toUpperCase "select * from"))
 
-  "But instance method calls are very different from normal functions"
+  ;; "But instance method calls are very different from normal functions"
+  "그러나 인스턴스 메소드를 호출하는 것은 보통의 함수를 부르는 것과 매우 다르다"
   (= ["SELECT" "FROM" "WHERE"] (map ___ ["select" "from" "where"]))
 
-  "Constructing might be harder than breaking"
+  ;; "Constructing might be harder than breaking"
+  "객체를 생성하는 일은 부수는 것보다 어려울 수 있다"
   (= 10 (let [latch (java.util.concurrent.CountDownLatch. __)]
           (.getCount latch)))
 
-  "Static methods are slashing prices!"
+  ;; "Static methods are slashing prices!"
+  "정적 메소드는 나눗셈 부호(/)를 쓴다"
   (== __ (Math/pow 2 10)))
--- src/koans/21_group_by.clj
+++ src/koans/21_group_by.clj
@@ -7,22 +7,26 @@
 
 (meditations
 
-  "To categorize a collection by some function, use group-by."
+  ;; "To categorize a collection by some function, use group-by."
+  "컬렉션을 어떤 함수로 분류하려면 group-by를 쓰라"
   (= __ (group-by count ["hello" "world" "foo" "bar"]))
 
 
-  "You can simulate filter + remove in one pass"
+  ;; "You can simulate filter + remove in one pass"
+  "filter와 remove를 한꺼번에 할 수 있다"
   (= (get-odds-and-evens [1 2 3 4 5])
      ((juxt filter remove) odd? [1 2 3 4 5])
      [[1 3 5] [2 4]])
 
-  "You can also group by a primary key"
+  ;; "You can also group by a primary key"
+  "기본 키(primary key) 기준으로 그룹을 나눌 수도 있다"
   (= __
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"} ]))
 
-  "But be careful when you group by non-required key"
+  ;; "But be careful when you group by non-required key"
+  "그러나 선택 키(non-required key)로 그룹을 나눌 때는 조심하라"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Mike" [{:name "Mike" :id 2}]
       __ [{:last-name "Smith" :id 1}]}
@@ -30,7 +34,8 @@
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"}]))
 
-  "The true power of group-by comes with custom functions"
+  ;; "The true power of group-by comes with custom functions"
+  "group-by의 진짜 힘은 맞춤(custom) 함수와 함께 나온다"
   (= __
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
