a81357afdf0d99619a337f9dec498e98f846e270
kwonhoe <kwonhoe1121@gmail.com>
2023-07-19 21:49:59 +0900

    clear 10_lazy_sequences.clj

--- src/koans/10_lazy_sequences.clj
+++ src/koans/10_lazy_sequences.clj
@@ -3,26 +3,26 @@
 
 (meditations
   "시퀀스를 만드는 방법은 여러 가지다"
-  (= __ (range 1 5))
+  (= '(1 2 3 4) (range 1 5))
 
   "범위는 기본적으로 0에서 시작한다"
-  (= __ (range 5))
+  (= (list 0 1 2 3 4) (range 5))
 
   "시퀀스가 클 때는 필요한 만큼만 가져오라"
   (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
+     (take 10 (range 100)))
 
   "아니면, 필요 없는 만큼을 버리고 쓰라"
   (= [95 96 97 98 99]
-     (drop __ (range 100)))
+     (drop 95 (range 100)))
 
   "iterate로 무한 지연(lazy) 시퀀스를 만들 수 있다"
-  (= __ (take 20 (iterate inc 0)))
+  (= (range 20) (take 20 (iterate inc 0)))
 
   "반복이 핵심이다"
   (= [:a :a :a :a :a :a :a :a :a :a ]
-     (repeat 10 __))
+     (repeat 10 :a))
 
   "반복하기 위해 iterate를 쓸 수 있다"
   (= (repeat 100 :foo)
-     (take 100 (iterate ___ :foo))))
+     (take 100 (iterate identity :foo))))
